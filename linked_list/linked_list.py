58452f97970c17298639cbe633b28f1388d570f5
Pete DeBiase <pete@debiase.io>
2022-02-22 18:59:49 -0800

    Implement various linked list methods
    Insert, traverse, linear search, delete node, delete self.

--- linked_list/linked_list.py
+++ linked_list/linked_list.py
@@ -11,10 +11,102 @@ class SinglyLinkedList():
         self.head = None
         self.tail = None
 
-linked_list = SinglyLinkedList()
-node1 = Node(1)
-node2 = Node(2)
+    def __iter__(self):
+        node = self.head
+        while node:
+            yield node
+            node = node.next
 
-linked_list.head = node1
-linked_list.head.next = node2
-linked_list.tail = node2
+    def __str__(self):
+        return str([node.value for node in self])
+
+    def insert(self, value, location):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = self.tail = new_node
+        else:
+            if location == 0: # Insert at beginning
+                new_node.next = self.head
+                self.head = new_node
+            elif location == -1: # Insert at end
+                self.tail.next = new_node
+                self.tail = new_node
+            else: # Insert in middle
+                temp_node = self.head
+                index = 0
+                while index < location - 1:
+                    temp_node = temp_node.next
+                    index += 1
+                next_node = temp_node.next
+                temp_node.next = new_node
+                new_node.next = next_node
+
+                if temp_node == self.tail:
+                    self.tail = new_node
+
+    def traverse(self):
+        if self.head is None:
+            print("Linked list is empty.")
+        else:
+            [print(node.value) for node in self]
+
+    def search_linear(self, target_value):
+        if self.head is None:
+            print("Linked list is empty.")
+        else:
+            for node in self:
+                if node.value == target_value:
+                    return node
+            return "Specified value not found."
+
+    def delete_node(self, location):
+        if self.head is None:
+            print("Linked list is empty.")
+        else:
+            if location == 0: # Delete at beginning
+                if self.head == self.tail: # LL contains a single node
+                    self.head = self.tail = None
+                else: # LL contains multiple nodes
+                    self.head = self.head.next
+            elif location == -1: # Delete at end
+                if self.head == self.tail: # LL contains a single node
+                    self.head = self.tail = None
+                else: # LL contains multiple nodes
+                    for node in self:
+                        if node.next == self.tail: break
+                    node.next = None
+                    self.tail = node
+            else: # Delete in middle
+                temp_node = self.head
+                for i in range(location - 1):
+                    temp_node = temp_node.next
+                temp_node.next = temp_node.next.next
+
+    def delete(self):
+        self.head = self.tail = None
+
+linked_list = SinglyLinkedList()
+linked_list.insert(1, -1)
+linked_list.insert(2, -1)
+linked_list.insert(3, -1)
+linked_list.insert(4, -1)
+
+linked_list.insert(0, 0)
+
+linked_list.insert(555, 3)
+
+print(linked_list)
+linked_list.traverse()
+
+print(linked_list.search_linear(555))
+print(linked_list.search_linear(999))
+
+linked_list.delete_node(0)
+print(linked_list)
+linked_list.delete_node(-1)
+print(linked_list)
+linked_list.delete_node(2)
+print(linked_list)
+
+linked_list.delete()
+print(linked_list)
